382705c4779f7883bbd633620079904d83689794
Josuex123 <josue7pedro@gmail.com>
2025-03-24 12:20:42 -0400

    Crud de tutor legal

--- TisOlimpSansi_BackEnd/routes/api.php
+++ TisOlimpSansi_BackEnd/routes/api.php
@@ -4,8 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\NombreDeHU1\UserPruebaController;
+use App\Http\Controllers\TutorLegal\TutorLegalController; 
 
 Route::controller(UserPruebaController::class)->group(function(){
     Route::post('/agregarEjemplo', [UserPruebaController::class, 'store']);
+    
+});
+
+
+//registrarTutorLegal
+Route::controller(TutorLegalController::class)->group(function(){
+
+    Route::get('/tutoresLegales', [TutorLegalController::class, 'index']);
+    Route::post('/agregarTutorLegal', [TutorLegalController::class, 'store']);
+    Route::get('/tutorLegal/{id}', [TutorLegalController::class, 'show']);
+    Route::put('/actualizarTutorLegal/{id}', [TutorLegalController::class, 'update']);
+    Route::delete('/eliminarTutorLegal/{id}', [TutorLegalController::class, 'destroy']);
     
 });
