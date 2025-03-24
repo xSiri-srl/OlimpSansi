2aca264ec8bce15a3686d33f66a2e66f168fde83
Josuex123 <josue7pedro@gmail.com>
2025-03-24 12:20:42 -0400

    Crud de tutor legal

---
+++ TisOlimpSansi_BackEnd/app/Models/TutorLegal/TutorLegalModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\TutorLegal;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class TutorLegalModel extends Model
+{
+    use HasFactory;
+    protected $table = 'tutor_legal';
+    protected $fillable = [
+        'nombre',
+        'apellido_pa',
+        'apellido_ma',
+        'ci',
+        'complemento',
+        'correo',
+        'numero_celular',
+        'tipo',
+        'fecha_registro',
+        'fecha_actualizacion',
+    ];
+}
