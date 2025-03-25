f60cbebf55172a94b2df24e3b59487f7af08dedb
Josuex123 <josue7pedro@gmail.com>
2025-03-25 00:01:02 -0400

    crud de tutor academico

--- TisOlimpSansi_BackEnd/routes/api.php
+++ TisOlimpSansi_BackEnd/routes/api.php
@@ -4,8 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\NombreDeHU1\UserPruebaController;
+use App\Http\Controllers\TutorAcademico\TutorAcademicoController;
 
 Route::controller(UserPruebaController::class)->group(function(){
     Route::post('/agregarEjemplo', [UserPruebaController::class, 'store']);
+    
+});
+
+//registrarTutorAcademico
+Route::controller(TutorAcademicoController::class)->group(function(){
+
+    Route::get('/tutoresAcademicos', [TutorAcademicoController::class, 'index']);
+    Route::post('/agregarTutorAcademico', [TutorAcademicoController::class, 'store']);
+    Route::get('/tutorAcademico/{id}', [TutorAcademicoController::class, 'show']);
+    Route::put('/actualizarTutorAcademico/{id}', [TutorAcademicoController::class, 'update']);
+    Route::delete('/eliminarTutorAcademico/{id}', [TutorAcademicoController::class, 'destroy']);
     
 });
