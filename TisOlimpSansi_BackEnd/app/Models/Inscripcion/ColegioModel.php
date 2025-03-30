b5e9886af0430098f31a099c698f2de37950a1f5
AdrianaGut257 <adrianagutierrezmollo@gmail.com>
2025-03-29 22:50:30 -0400

    relaciones con llaves foraneas de estudainte, grado, colegio, tutor legal

--- TisOlimpSansi_BackEnd/app/Models/Inscripcion/ColegioModel.php
+++ TisOlimpSansi_BackEnd/app/Models/Inscripcion/ColegioModel.php
@@ -4,6 +4,7 @@ namespace App\Models\Inscripcion;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Inscripcion\EstudianteModel;
 
 class ColegioModel extends Model
 {
@@ -14,4 +15,8 @@ class ColegioModel extends Model
         'departamento',
         'provincia',
     ];
+
+    public function estudiante(){
+        return $this->hasOne(EstudianteModel::class). 'id_unidad';
+    }
 }
