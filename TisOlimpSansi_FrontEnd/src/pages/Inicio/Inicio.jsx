f9ba59cb5f1704b5035d6ac771faa822ece4afb1
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2025-04-08 17:05:39 -0400

    Style: cambiar nombre y logo del Nabvar

--- TisOlimpSansi_FrontEnd/src/pages/Inicio/Inicio.jsx
+++ TisOlimpSansi_FrontEnd/src/pages/Inicio/Inicio.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Inicio = () => {
   return (
     <div className="h-screen flex items-center justify-center bg-gradient-to-r from-blue-700 to-red-700">
-     <p class="font-mono text-white text-7xl" >Bienvenido a OlimpSansi</p>
+     <p class="font-mono text-white text-7xl" >Bienvenido a  </p>
+     <img
+              src="/images/Ohsansi_Logo.png"
+              alt="OlimpSansi Logo"
+              className="h-13 w-auto"
+            />
       
     </div>
   );
