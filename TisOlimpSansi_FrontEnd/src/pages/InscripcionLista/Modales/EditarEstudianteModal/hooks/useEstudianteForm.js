0705311c0a3814c34190bf7c10b22f18878e494e
Faxterefalt <azugafabian321@gmail.com>
2025-04-12 16:24:14 -0400

    cambio de estructura

--- TisOlimpSansi_FrontEnd/src/pages/InscripcionLista/Modales/EditarEstudianteModal/hooks/useEstudianteForm.js
+++ TisOlimpSansi_FrontEnd/src/pages/InscripcionLista/Modales/EditarEstudianteModal/hooks/useEstudianteForm.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import { SECCIONES } from './../EditarEstudianteModal/constants'
-import { normalizarDatosEstudiante } from './../EditarEstudianteModal/utils';
+import { SECCIONES } from '../constants'
+import { normalizarDatosEstudiante } from '../utils';
 
 export const useEstudianteForm = (estudiante) => {
   const [seccionActiva, setSeccionActiva] = useState(SECCIONES.TODOS);
