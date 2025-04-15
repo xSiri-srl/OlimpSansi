52c6b672cd769adff5dabf897aee313ee7550523
jeremiasVA <eljerevizcarra@gmail.com>
2025-04-14 23:08:55 -0400

    bug en responsable inscripcion

--- TisOlimpSansi_FrontEnd/src/pages/OrdenDePago/InscripcionResponsable.jsx
+++ TisOlimpSansi_FrontEnd/src/pages/OrdenDePago/InscripcionResponsable.jsx
@@ -248,20 +248,20 @@ const ResponsableForm = ({ formData, handleInputChange, handleNext }) => {
               isSubmitting ||
               !formData.responsable?.nombres ||
               !formData.responsable?.ci ||
-              formData.responsable?.ci.length < 7 ||
-              formData.responsable?.nombres.length < 2 ||
-              formData.responsable?.apellidoMaterno.length < 2 ||
-              formData.responsable?.apellidoPaterno.length < 2 ||
-              formData.responsable?.nombres.split(" ").length > 2
+              formData.responsable?.ci?.length < 7 ||
+              formData.responsable?.nombres?.length < 2 ||
+              formData.responsable?.apellidoMaterno?.length < 2 ||
+              formData.responsable?.apellidoPaterno?.length < 2 ||
+              formData.responsable?.nombres?.split(" ").length > 2
             }
             className={`px-6 py-2 transition duration-300 ease-in-out text-white rounded-md shadow-md ${
               formData.responsable?.nombres &&
               formData.responsable?.ci &&
-              formData.responsable?.ci.length >= 7 &&
-              formData.responsable?.nombres.length >= 2 &&
-              formData.responsable?.apellidoMaterno.length >= 2 &&
-              formData.responsable?.apellidoPaterno.length >= 2 &&
-              formData.responsable?.nombres.split(" ").length <= 2 &&
+              formData.responsable?.ci?.length >= 7 &&
+              formData.responsable?.nombres?.length >= 2 &&
+              formData.responsable?.apellidoMaterno?.length >= 2 &&
+              formData.responsable?.apellidoPaterno?.length >= 2 &&
+              formData.responsable?.nombres?.split(" ").length <= 2 &&
               !isSubmitting
                 ? "bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
                 : "bg-gray-400 cursor-not-allowed"
