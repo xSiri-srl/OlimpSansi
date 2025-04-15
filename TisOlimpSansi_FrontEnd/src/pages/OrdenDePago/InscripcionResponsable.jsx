7844f27677c78300172a37213d3b5c7b6bbaa27d
Adriana G <adrianagutierrezmollo@gmail.com>
2025-04-14 20:43:44 -0400

    ojo

--- TisOlimpSansi_FrontEnd/src/pages/OrdenDePago/InscripcionResponsable.jsx
+++ TisOlimpSansi_FrontEnd/src/pages/OrdenDePago/InscripcionResponsable.jsx
@@ -249,6 +249,8 @@ const ResponsableForm = ({ formData, handleInputChange, handleNext }) => {
             className={`px-6 py-2 transition duration-300 ease-in-out text-white rounded-md shadow-md ${
               formData.responsable?.nombres &&
               formData.responsable?.ci &&
+              formData.responsable?.apellidoPaterno &&
+              formData.responsable?.apellidoMaterno &&
               !isSubmitting
                 ? "bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
                 : "bg-gray-400 cursor-not-allowed"
