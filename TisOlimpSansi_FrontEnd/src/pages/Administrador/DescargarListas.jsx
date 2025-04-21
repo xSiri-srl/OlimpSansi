6a6f1cd3dffdd0314a1ab3d3422218e1814e8915
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2025-04-20 23:03:22 -0400

    Conectar back con frotn pero con fallas

--- TisOlimpSansi_FrontEnd/src/pages/Administrador/DescargarListas.jsx
+++ TisOlimpSansi_FrontEnd/src/pages/Administrador/DescargarListas.jsx
@@ -68,153 +68,7 @@ const datos = {
   ],
 };
 
-const listaInscritos = [
-  {
-    apellidoPaternoEstudiante: "Ramirez",
-    apellidoMaternoEstudiante: "Fernandez",
-    nombresEstudiante: "Lucia Mariana",
-    carnetDeIdentidadEstudiante: "7865432",
-    fechaNacimiento: "2010-05-15",
-    correoElectronicoEstudiante: "lucia.mf@gmail.com",
-    correoPertenece: "Estudiante",
-    curso: "3ro Primaria",
-    area: "astronomia",
-    categoria: "3P",
-    colegio: "San Agustin",
-    departamento: "Coachabamba",
-    provincia: "Cercado",
 
-    rolDelTutor: "Tutor",
-    apellidoPaterno: "Guzman",
-    apellidoMaterno: "Torrez",
-    nombres: "Eduardo",
-    carnetDeIdentidad: "1234567890",
-    correoElectronico: "guzman@email.com",
-    telefonoCelular: "+591 71122334",
-
-    apellidoPaternoTutorAcademico: "Martinez",
-    apellidoMaternoTutorAcademico: "Paredes",
-    nombresTutorAcademico: "Juan Pablo",
-    carnetDeIdentidadTutorAcademico: "0987654321",
-    correoElectronicoTutorAcademico: "juan.martinez@email.com",
-  },
-  {
-    apellidoPaternoEstudiante: "Salazar",
-    apellidoMaternoEstudiante: "Gomez",
-    nombresEstudiante: "Mateo Andres",
-    carnetDeIdentidadEstudiante: "7654321",
-    fechaNacimiento: "2011-07-20",
-    correoElectronicoEstudiante: "mateo.salazar@gmail.com",
-    correoPertenece: "Estudiante",
-    curso: "3ro Primaria",
-    area: "fisica",
-    categoria: "3P",
-    colegio: "San Agustin",
-    departamento: "Cochabamba",
-    provincia: "Cercado",
-
-    rolDelTutor: "Padre",
-    apellidoPaterno: "Salazar",
-    apellidoMaterno: "Ruiz",
-    nombres: "Fernando Javier",
-    carnetDeIdentidad: "88776655",
-    correoElectronico: "fernando.salazar@email.com",
-    telefonoCelular: "+591 71011223",
-
-    apellidoPaternoTutorAcademico: "Chavez",
-    apellidoMaternoTutorAcademico: "Luna",
-    nombresTutorAcademico: "Natalia Sofia",
-    carnetDeIdentidadTutorAcademico: "22114433",
-    correoElectronicoTutorAcademico: "natalia.chavez@email.com",
-  },
-  {
-    apellidoPaternoEstudiante: "Rojas",
-    apellidoMaternoEstudiante: "Llanos",
-    nombresEstudiante: "Camila Renata",
-    carnetDeIdentidadEstudiante: "8765432",
-    fechaNacimiento: "2010-09-10",
-    correoElectronicoEstudiante: "camila.rojas@gmail.com",
-    correoPertenece: "Estudiante",
-    curso: "4to Primaria",
-    area: "astronomia",
-    categoria: "3P",
-    colegio: "Santa Ana",
-    departamento: "Cochabamba",
-    provincia: "Cercado",
-
-    rolDelTutor: "Tutor",
-    apellidoPaterno: "Rojas",
-    apellidoMaterno: "Mendoza",
-    nombres: "Javier Leonel",
-    carnetDeIdentidad: "99887766",
-    correoElectronico: "javier.rojas@email.com",
-    telefonoCelular: "+591 72445566",
-
-    apellidoPaternoTutorAcademico: "Paz",
-    apellidoMaternoTutorAcademico: "Arce",
-    nombresTutorAcademico: "Rodrigo Esteban",
-    carnetDeIdentidadTutorAcademico: "33446677",
-    correoElectronicoTutorAcademico: "rodrigo.paz@email.com",
-  },
-  {
-    apellidoPaternoEstudiante: "Gutierrez",
-    apellidoMaternoEstudiante: "Medina",
-    nombresEstudiante: "Sofia Alejandra",
-    carnetDeIdentidadEstudiante: "9988221",
-    fechaNacimiento: "2012-01-25",
-    correoElectronicoEstudiante: "sofia.gutierrez@gmail.com",
-    correoPertenece: "Estudiante",
-    curso: "3ro Primaria",
-    area: "astronomia",
-    categoria: "3P",
-    colegio: "San Agustin",
-    departamento: "Cochabamba",
-    provincia: "Cercado",
-
-    rolDelTutor: "Tutor",
-    apellidoPaterno: "Gutierrez",
-    apellidoMaterno: "Sanchez",
-    nombres: "Carlos Ernesto",
-    carnetDeIdentidad: "55667788",
-    correoElectronico: "carlos.gutierrez@email.com",
-    telefonoCelular: "+591 73334455",
-
-    apellidoPaternoTutorAcademico: "Vargas",
-    apellidoMaternoTutorAcademico: "Delgado",
-    nombresTutorAcademico: "Daniela Fernanda",
-    carnetDeIdentidadTutorAcademico: "22331144",
-    correoElectronicoTutorAcademico: "daniela.vargas@email.com",
-  },
-  {
-    apellidoPaternoEstudiante: "Lopez",
-    apellidoMaternoEstudiante: "Carrasco",
-    nombresEstudiante: "Diego Armando",
-    carnetDeIdentidadEstudiante: "5566778",
-    fechaNacimiento: "2011-04-18",
-    correoElectronicoEstudiante: "diego.lopez@gmail.com",
-    correoPertenece: "Estudiante",
-    curso: "3ro Primaria",
-    area: "astronomia",
-    categoria: "5P",
-    colegio: "San Agustin",
-    departamento: "Cochabamba",
-    provincia: "Cercado",
-
-    rolDelTutor: "Tutor",
-    apellidoPaterno: "Lopez",
-    apellidoMaterno: "Villarroel",
-    nombres: "Marcos Ivan",
-    carnetDeIdentidad: "44556677",
-    correoElectronico: "marcos.lopez@email.com",
-    telefonoCelular: "+591 74455667",
-
-    apellidoPaternoTutorAcademico: "Peña",
-    apellidoMaternoTutorAcademico: "Rivera",
-    nombresTutorAcademico: "Angela Marcela",
-    carnetDeIdentidadTutorAcademico: "11223344",
-    correoElectronicoTutorAcademico: "angela.pena@email.com",
-  },
-];
 
 function DescargarListas() {
   const [area, setArea] = useState("");
@@ -225,15 +79,26 @@ function DescargarListas() {
   const [colegio, setColegio] = useState("");
   const [fecha, setFecha] = useState("");
 
-  const resultadosFiltrados = listaInscritos.filter((item) => {
+
+  const [inscritos, setInscritos] = useState([]);
+ 
+
+  useEffect(() => {
+    axios.get("/lista-inscritos").then((response) => {
+      setInscritos(response.data); 
+    });
+  }, []);
+
+  const resultadosFiltrados = inscritos.filter((inscrito) => {
     return (
-      (!area || item.area === area) &&
-      (!curso || item.curso === curso) &&
-      (!categoria || item.categoria === categoria) &&
-      (!colegio || item.colegio === colegio) &&
-      (!fecha || item.fecha === fecha)
+      (area === "" || inscrito.nombre_area === area) &&
+      (curso === "" || inscrito.curso === curso) &&
+      (categoria === "" || inscrito.categoria === categoria) &&
+      (colegio === "" || inscrito.colegio === colegio) &&
+      (fecha === "" || inscrito.fecha_nacimiento === fecha)
     );
   });
+  
 
   const descargarPDF = () => {
     const doc = new jsPDF();
@@ -279,15 +144,12 @@ function DescargarListas() {
       <h1 className="text-sky-950 font-bold text-3xl mb-6 p-6 text-center">
         Descargar lista de inscritos
       </h1>
-
+  
       <div className="w-full max-w-4xl mx-auto bg-sky-50 p-6 rounded-2xl shadow-lg">
         <div className="flex flex-col md:flex-row gap-4">
           {/* Área */}
           <div className="flex-1">
-            <label
-              htmlFor="area"
-              className="block mb-2 text-sm font-semibold text-gray-700"
-            >
+            <label htmlFor="area" className="block mb-2 text-sm font-semibold text-gray-700">
               Área
             </label>
             <select
@@ -305,13 +167,10 @@ function DescargarListas() {
               ))}
             </select>
           </div>
-
+  
           {/* Curso */}
           <div className="flex-1">
-            <label
-              htmlFor="curso"
-              className="block mb-2 text-sm font-semibold text-gray-700"
-            >
+            <label htmlFor="curso" className="block mb-2 text-sm font-semibold text-gray-700">
               Curso
             </label>
             <select
@@ -322,22 +181,17 @@ function DescargarListas() {
               className="block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-sm text-gray-700"
             >
               <option value="">-- Selecciona --</option>
-              {[...new Set(categorias.map((cat) => cat.split(" - ")[1]))].map(
-                (curso, index) => (
-                  <option key={index} value={curso}>
-                    {curso}
-                  </option>
-                )
-              )}
+              {[...new Set(categorias.map((cat) => cat.split(" - ")[1]))].map((curso, index) => (
+                <option key={index} value={curso}>
+                  {curso}
+                </option>
+              ))}
             </select>
           </div>
-
+  
           {/* Categoría */}
           <div className="flex-1">
-            <label
-              htmlFor="categoria"
-              className="block mb-2 text-sm font-semibold text-gray-700"
-            >
+            <label htmlFor="categoria" className="block mb-2 text-sm font-semibold text-gray-700">
               Categoría
             </label>
             <select
@@ -355,13 +209,10 @@ function DescargarListas() {
               ))}
             </select>
           </div>
-
+  
           {/* colegio */}
           <div className="flex-1">
-            <label
-              htmlFor="colegio"
-              className="block mb-2 text-sm font-semibold text-gray-700"
-            >
+            <label htmlFor="colegio" className="block mb-2 text-sm font-semibold text-gray-700">
               Colegio
             </label>
             <select
@@ -372,15 +223,14 @@ function DescargarListas() {
               onChange={(e) => setColegio(e.target.value)}
             >
               <option value="">-- Selecciona --</option>
-              {[...new Set(listaInscritos.map((item) => item.colegio))].map(
-                (colegioName, idx) => (
-                  <option key={idx} value={colegioName}>
-                    {colegioName}
-                  </option>
-                )
-              )}
+              {[...new Set(inscritos.map((item) => item.colegio))].map((colegioName, idx) => (
+                <option key={idx} value={colegioName}>
+                  {colegioName}
+                </option>
+              ))}
             </select>
           </div>
+  
           <div className="flex-1">
             <label className="flex items-center gap-2 text-sm font-semibold text-gray-700 mb-2">
               Fecha
@@ -393,29 +243,20 @@ function DescargarListas() {
           </div>
         </div>
       </div>
-
+  
       {resultadosFiltrados.length > 0 ? (
         <div className="mt-6 flex justify-center">
           <div className="overflow-x-auto w-full max-w-6xl">
-            <table class="min-w-max border border-gray-300 text-sm text-left">
+            <table className="min-w-max border border-gray-300 text-sm text-left">
               <thead>
                 <tr>
-                  <th
-                    colSpan="13"
-                    className="py-2 px-4 border border-gray-300 font-semibold bg-green-200 text-center"
-                  >
+                  <th colSpan="13" className="py-2 px-4 border border-gray-300 font-semibold bg-green-200 text-center">
                     Datos del competidor
                   </th>
-                  <th
-                    colSpan="7"
-                    className="py-2 px-4 border border-gray-300 font-semibold bg-blue-200 text-center"
-                  >
+                  <th colSpan="7" className="py-2 px-4 border border-gray-300 font-semibold bg-blue-200 text-center">
                     Datos del tutor legal
                   </th>
-                  <th
-                    colSpan="5"
-                    className="py-2 px-4 border border-gray-300 font-semibold bg-purple-200 text-center"
-                  >
+                  <th colSpan="5" className="py-2 px-4 border border-gray-300 font-semibold bg-purple-200 text-center">
                     Datos del tutor académico
                   </th>
                 </tr>
@@ -451,7 +292,7 @@ function DescargarListas() {
                     if (idx < 13) bgColor = "bg-green-100";
                     else if (idx < 20) bgColor = "bg-blue-100";
                     else bgColor = "bg-purple-100";
-
+  
                     return (
                       <th
                         key={idx}
@@ -464,71 +305,33 @@ function DescargarListas() {
                 </tr>
               </thead>
               <tbody>
-                {resultadosFiltrados.map((inscrito, index) => (
+                {resultadosFiltrados.map((inscritos, index) => (
                   <tr key={index} className="hover:bg-gray-50">
-                    <td className="px-4 py-2 border">
-                      {inscrito.apellidoPaternoEstudiante}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.apellidoMaternoEstudiante}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.nombresEstudiante}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.carnetDeIdentidadEstudiante}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.fechaNacimiento}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.correoElectronicoEstudiante}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.correoPertenece}
-                    </td>
-                    <td className="px-4 py-2 border">{inscrito.curso}</td>
-                    <td className="px-4 py-2 border">{inscrito.area}</td>
-                    <td className="px-4 py-2 border">{inscrito.categoria}</td>
-                    <td className="px-4 py-2 border">{inscrito.colegio}</td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.departamento}
-                    </td>
-                    <td className="px-4 py-2 border">{inscrito.provincia}</td>
-
-                    <td className="px-4 py-2 border">{inscrito.rolDelTutor}</td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.apellidoPaterno}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.apellidoMaterno}
-                    </td>
-                    <td className="px-4 py-2 border">{inscrito.nombres}</td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.carnetDeIdentidad}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.correoElectronico}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.telefonoCelular}
-                    </td>
-
-                    <td className="px-4 py-2 border">
-                      {inscrito.apellidoPaternoTutorAcademico}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.apellidoMaternoTutorAcademico}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.nombresTutorAcademico}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.carnetDeIdentidadTutorAcademico}
-                    </td>
-                    <td className="px-4 py-2 border">
-                      {inscrito.correoElectronicoTutorAcademico}
-                    </td>
+                    <td className="px-4 py-2 border">{inscritos.apellido_pa}</td>
+                    <td className="px-4 py-2 border">{inscritos.apellido_ma}</td>
+                    <td className="px-4 py-2 border">{inscritos.nombre}</td>
+                    <td className="px-4 py-2 border">{inscritos.ci}</td>
+                    <td className="px-4 py-2 border">{inscritos.fecha_nacimiento}</td>
+                    <td className="px-4 py-2 border">{inscritos.correo}</td>
+                    <td className="px-4 py-2 border">{inscritos.propietario_correo}</td>
+                    <td className="px-4 py-2 border">{inscritos.curso}</td>
+                    <td className="px-4 py-2 border">{inscritos.nombre_area}</td>
+                    <td className="px-4 py-2 border">{inscritos.categoria}</td>
+                    <td className="px-4 py-2 border">{inscritos.colegio}</td>
+                    <td className="px-4 py-2 border">{inscritos.departamento}</td>
+                    <td className="px-4 py-2 border">{inscritos.provincia}</td>
+                    <td className="px-4 py-2 border">{inscritos.rol_tutor_legal}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_legal_apellido_pa}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_legal_apellido_ma}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_legal_nombre}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_legal_ci}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_legal_correo}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_legal_telefono}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_academico_apellido_pa}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_academico_apellido_ma}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_academico_nombre}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_academico_ci}</td>
+                    <td className="px-4 py-2 border">{inscritos.tutor_academico_correo}</td>
                   </tr>
                 ))}
               </tbody>
@@ -536,29 +339,28 @@ function DescargarListas() {
           </div>
         </div>
       ) : (
-        <p className="mt-6 text-center text-gray-600">
-          No se encontraron resultados.
-        </p>
+        <p className="mt-6 text-center text-gray-600">No se encontraron resultados.</p>
       )}
-
-{resultadosFiltrados.length > 0 && (
-  <div className="flex justify-end mt-6 gap-2 mb-4">
-    <button
-      onClick={descargarPDF}
-      className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-    >
-      Descargar PDF
-    </button>
-    <button
-      onClick={descargarExcel}
-      className="bg-green-500 text-white px-4 mr-9 py-2 rounded hover:bg-green-600"
-    >
-      Descargar Excel
-    </button>
-  </div>
-)}
+  
+      {resultadosFiltrados.length > 0 && (
+        <div className="flex justify-end mt-6 gap-2 mb-4">
+          <button
+            onClick={descargarPDF}
+            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+          >
+            Descargar PDF
+          </button>
+          <button
+            onClick={descargarExcel}
+            className="bg-green-500 text-white px-4 mr-9 py-2 rounded hover:bg-green-600"
+          >
+            Descargar Excel
+          </button>
+        </div>
+      )}
     </div>
   );
+  
 }
 
 export default DescargarListas;
