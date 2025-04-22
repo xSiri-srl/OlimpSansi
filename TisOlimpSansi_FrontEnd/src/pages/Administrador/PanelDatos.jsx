8e42a19542f30461f06244a8b80a5c135e8c8abb
Faxter <fabianazuga123hotmail.com>
2025-04-22 14:00:10 -0400

    cambio de ruta

---
+++ TisOlimpSansi_FrontEnd/src/pages/Administrador/PanelDatos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PanelDatos = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold">Panel de Datos del Administrador</h1>
+    </div>
+  );
+};
+
+export default PanelDatos;
