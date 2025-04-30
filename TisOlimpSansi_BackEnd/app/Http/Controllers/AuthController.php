8918b2e2758064a48f4920c34dc58377947fa13f
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-04-30 09:36:51 -0400

    add cors

--- TisOlimpSansi_BackEnd/app/Http/Controllers/AuthController.php
+++ TisOlimpSansi_BackEnd/app/Http/Controllers/AuthController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
 
     public function register(RegistroRequest $request){
-        
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
     }
     public function login(LoginRequest $request){
         $data = $request->validated();
