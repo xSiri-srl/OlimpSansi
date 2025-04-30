724e5307ddf918f5bbcf14a92c94154d8dde0830
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-04-30 09:11:00 -0400

    agregar tabla users

--- TisOlimpSansi_BackEnd/app/Http/Controllers/AuthController.php
+++ TisOlimpSansi_BackEnd/app/Http/Controllers/AuthController.php
@@ -8,6 +8,19 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function login(LoginRequest $request){
+        $data = $request->validated();
+        //Revisar parssword
+        if(!Auth::attemp($data)) {
+            return response([
+                'errors' => ['El email o el password son incorrectos']
+            ],422);
+        }
+        //Autenticar usuario
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
 
     }
     public function logout(Request $request){
