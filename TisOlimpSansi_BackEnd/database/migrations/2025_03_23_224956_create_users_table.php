699a671b63929c58be95bb61d46c202e18deedde
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-04-30 09:11:00 -0400

    agregar tabla users

--- TisOlimpSansi_BackEnd/database/migrations/2025_03_23_224956_create_users_table.php
+++ TisOlimpSansi_BackEnd/database/migrations/2025_03_23_224956_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users_prueba', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users_prueba');
+        Schema::dropIfExists('users');
     }
 };
