6e320ea853d83d331f0b39222dcc817dc60d7043
jeremiasVA <150405774+jeremiasVA@users.noreply.github.com>
2025-05-07 09:59:27 -0400

    controladores nuevas tablas

--- TisOlimpSansi_BackEnd/app/Http/Controllers/OlimpiadaAreaController.php
+++ TisOlimpSansi_BackEnd/app/Http/Controllers/OlimpiadaAreaController.php
@@ -7,5 +7,61 @@ use Illuminate\Http\Request;
 
 class OlimpiadaAreaController extends Controller
 {
-    //
+    public function index()
+    {
+        $relaciones = OlimpiadaAreaModel::all();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $relaciones,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $relacion = new OlimpiadaAreaModel();
+        $relacion->id_olimpiada = $request->id_olimpiada;
+        $relacion->id_area = $request->id_area;
+        $relacion->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Área vinculada a olimpiada exitosamente',
+        ]);
+    }
+
+    public function show($id)
+    {
+        $relacion = OlimpiadaAreaModel::findOrFail($id);
+
+        return response()->json([
+            'status' => 200,
+            'data' => $relacion,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $relacion = OlimpiadaAreaModel::findOrFail($id);
+        $relacion->id_olimpiada = $request->id_olimpiada;
+        $relacion->id_area = $request->id_area;
+        $relacion->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Relación actualizada exitosamente',
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $relacion = OlimpiadaAreaModel::findOrFail($id);
+        $relacion->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Relación eliminada exitosamente',
+        ]);
+    }
+
 }
