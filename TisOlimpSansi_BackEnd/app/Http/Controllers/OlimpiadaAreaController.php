97b36b3b940a7c9232f84532d4c2fcf5b5ec403a
Josuex123 <josue7pedro@gmail.com>
2025-05-08 16:46:53 -0400

    arreglito del monto

--- TisOlimpSansi_BackEnd/app/Http/Controllers/OlimpiadaAreaController.php
+++ TisOlimpSansi_BackEnd/app/Http/Controllers/OlimpiadaAreaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\OlimpiadaAreaModel;
 
 class OlimpiadaAreaController extends Controller
 {
@@ -44,8 +45,6 @@ class OlimpiadaAreaController extends Controller
     public function update(Request $request, $id)
     {
         $relacion = OlimpiadaAreaModel::findOrFail($id);
-        $relacion->id_olimpiada = $request->id_olimpiada;
-        $relacion->id_area = $request->id_area;
         $relacion->monto = $request->monto;
         $relacion->save();
 
