80719bc554ca4f6bf9e5d15bf29f68f455ab5685
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-05-08 22:02:34 -0400

    add atributos olimpiada area categoria

--- TisOlimpSansi_BackEnd/database/migrations/2025_05_09_015930_create_olimpiada_area_categorias_table.php
+++ TisOlimpSansi_BackEnd/database/migrations/2025_05_09_015930_create_olimpiada_area_categorias_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('olimpiada_area_categorias', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_olimpiada')->constrained('olimpiada')->onDelete('cascade');
+            $table->foreignId('id_area')->constrained('area')->onDelete('cascade');
+            $table->foreignId('id_categoria')->constrained('categoria')->onDelete('cascade');
+            $table->decimal('precio', 10, 2);
             $table->timestamps();
+            
+            $table->unique(['id_olimpiada', 'id_area', 'id_categoria'], 'olimpiada_area_categoria_unique');
         });
     }
 
