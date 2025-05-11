861b545910223c22fc05d2eae02814652e35200a
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-05-10 20:33:17 -0400

    seeder de combinatori de areas y categorias

--- TisOlimpSansi_BackEnd/database/seeders/DatabaseSeeder.php
+++ TisOlimpSansi_BackEnd/database/seeders/DatabaseSeeder.php
@@ -34,5 +34,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GradoSeeder::class,
         ]);
+        $this->call([
+            OlimpiadaAreaCategoriaSeeder::class
+        ]);
     }
 }
