936a482e4ce15d9320a3e525c49d989c7554721f
jeremiasVA <150405774+jeremiasVA@users.noreply.github.com>
2025-05-11 19:37:00 -0400

    guardarComprobante ahora esta en ComprobanteController

--- TisOlimpSansi_BackEnd/app/Http/Controllers/OrdenPagoController.php
+++ TisOlimpSansi_BackEnd/app/Http/Controllers/OrdenPagoController.php
@@ -195,40 +195,6 @@ class OrdenPagoController extends Controller
         ]);
     }
 
-
-    public function guardarComprobante(Request $request)
-    {
-        $validated = $request->validate([
-            'numero_comprobante' => 'required|string',
-            'codigo_generado' => 'required|string',
-            'comprobante' => 'required|image|mimes:jpg,png,jpeg|max:5120',
-            'nombre_pagador' => 'required|string',
-        ]);
-
-        $imagen = $request->file('comprobante');
-        $comprobantePath = $imagen->store('comprobantes', 'public');
-
-        $ordenPago = OrdenPago::where('codigo_generado', $validated['codigo_generado'])->first();
-
-        if (!$ordenPago) {
-            return response()->json(['message' => 'Código no encontrado.'], 404);
-        }
-
-        // Crear un nuevo comprobante relacionado a la orden de pago
-        $comprobante = new comprobantes_pago();
-        $comprobante->id_orden_pago = $ordenPago->id;
-        $comprobante->numero_comprobante = $validated['numero_comprobante'];
-        $comprobante->comprobante_url = $comprobantePath;
-        $comprobante->fecha_subida_imagen_comprobante = now();
-        $comprobante->nombre_pagador = $validated['nombre_pagador'];
-        $comprobante->save();
-
-        return response()->json([
-            'message' => 'Comprobante guardado exitosamente',
-            'comprobante' => $comprobante
-        ]);
-    }
-
     /**
      * Obtiene la orden de pago por su código generado
      */
