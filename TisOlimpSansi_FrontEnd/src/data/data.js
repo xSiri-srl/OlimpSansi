b0ccb5e6fbd1bd76d1b6d25e4534f0b882abb164
Faxterefalt <azugafabian321@gmail.com>
2025-05-19 22:19:14 -0400

    entre otras muchas cosas

--- TisOlimpSansi_FrontEnd/src/data/data.js
+++ TisOlimpSansi_FrontEnd/src/data/data.js
@@ -14,11 +14,13 @@ export const navbarLinksByRole = {
     //{ id: 2, title: "Subir Convocatoria", link: "/admin/convocatoria" },
   ],
   admin: [
-    //{ id: 1, title: "Generar Reportes", link: "/admin/generar-reportes" },
     { id: 1, title: "Inicio", link: "/" },
-    { id: 2, title: "Crear Olimpiada", link: "/admin/crear-olimpiada" },
-    { id: 3, title: "Asignar Áreas", link: "/admin/asociar-nivel" },
-    { id: 4, title: "Asignar Costos", link: "/admin/asociar-costo" },
-    //{ id: 2, title: "Subir Convocatoria", link: "/admin/convocatoria" },
+   { id: 2, title: "Generar Reportes", link: "/admin/generar-reportes" },
+    { id: 3, title: "Crear Olimpiada", link: "/admin/crear-olimpiada" },
+    { id: 4, title: "Asignar Áreas", link: "/admin/asociar-nivel" },
+    { id: 5, title: "Asignar Costos", link: "/admin/asociar-costo" },
+    { id: 6, title: "Limitar Inscripciones", link: "/admin/asociar-limite" },
+    { id: 7, title: "Desasignar Áreas", link: "/admin/des-asignar-costo" },
+  //  { id: 8, title: "Convocatorias", link: "/admin/convocatoria" },
   ],
 };
